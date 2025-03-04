66152eafcdf687b7dd3175ab0be153fb9a4489ce
Simon Hermann <148443193+ratsetflummi@users.noreply.github.com>
2025-03-04 13:50:50 +0100

    Working Base

---
+++ RKOMBegehungsExcelUpdater.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import filedialog as fd
+from openpyxl import load_workbook
+
+
+from planio.planio_queries import (
+    get_begehungsdaten
+)
+
+api_key = "547fa8685b2e0a692cdeb33aeda305edd27b0155"
+
+def main():
+    # def handle_boolean(value):
+    #     if isinstance(value, bool):  # If it's already a boolean, return it as is
+    #         return value
+    #     elif isinstance(value, str):  # If it's a string
+    #         # Convert to lowercase and check if it's 'true' or 'false'
+    #         value = value.strip().lower()  # Strip to remove extra spaces and convert to lowercase
+    #         if value == 'wahr':
+    #             return True
+    #         elif value == 'falsch':
+    #             return False
+    def select_file():
+        output_label.configure(text="",bg="white")
+        file_label.configure(bg="white")
+        filename.configure(bg="white")
+        root.configure(bg="white")
+        filename.configure(text=fd.askopenfilename())
+    def update_file(filename):
+        if filename.cget("text").endswith(".xlsx"):
+            output_label.configure(text="Fehler (Zieldatei muss geschlossen sein)",bg="orange")
+            file_label.configure(bg="orange")
+            filename.configure(bg="orange")
+            root.configure(bg="orange")
+            begehungsdaten = get_begehungsdaten(api_key,'131')
+            df = pd.read_excel(filename.cget("text"))
+
+            # Iterate through each row
+            for index, row in df.iterrows():
+                # You can access individual values by column name
+                if(not pd.isnull(row['Strasse'])):
+                    zusatz = ""
+                    if(not pd.isnull(row['Hhnrzusatz'])):
+                        zusatz = " " + row['Hhnrzusatz']
+                    adresse = str(row['Gfrgebaeudeid']) + " - " + row['Strasse'] + " " +str(int(row['Hhnr'])) + zusatz + ", " + str(int(row['Plz'])) + " " + row['Ort']
+                    planio_row = begehungsdaten[begehungsdaten["address"] == adresse].head(1)
+                    if not planio_row.empty:
+                        df.loc[index, "Sachstand"] = planio_row["sachstand"].iloc[0]
+                        if not pd.isnull(row["Erschließung-Bemerkung"]):
+                            df.loc[index, "Erschließung-Bemerkung"] = row["Erschließung-Bemerkung"] + " + " + planio_row["bemerkung"].iloc[0]
+                        else:
+                            df.loc[index, "Erschließung-Bemerkung"] = planio_row["bemerkung"].iloc[0]
+                        # if handle_boolean(row["Nutzungsvereinbarung"]) and planio_row["status"].iloc[0] == "Wartend":
+                        #     print("change" + str(planio_row["issue_id"].iloc[0]))
+                            
+            
+            # print("Planio________________________________")
+            # print(df)
+            #print(begehungsdaten)
+            statuses = []
+            for index,row in begehungsdaten.iterrows():
+                if not row["status"] in statuses:
+                    statuses.append(row["status"])
+                #print(row)
+                #print(statuses)
+            
+            # Load the existing workbook
+            book = load_workbook(filename.cget("text")) #filename.cget("text")
+            sheet = book.worksheets[0]
+            for row_num, row in enumerate(df.values,2): 
+                for col_num, value in enumerate(row, start=1):
+                    sheet.cell(row=row_num, column=col_num, value=value)
+            book.save(filename.cget("text")) #filename.cget("text")
+            output_label.configure(text="Fertig",bg="lightgreen")
+            file_label.configure(bg="lightgreen")
+            filename.configure(bg="lightgreen")
+            root.configure(bg="lightgreen")
+        else:
+            output_label.configure(text="Datei nicht kompatibel",bg="yellow")
+            file_label.configure(bg="yellow")
+            filename.configure(bg="yellow")
+            root.configure(bg="yellow")
+
+    def on_resize(event):
+        file_label.configure(wraplength=root.winfo_width()-50)
+        output_label.configure(wraplength=root.winfo_width()-50)
+        filename.configure(wraplength=root.winfo_width()-50)
+
+    # Create the main window
+    root = tk.Tk()
+    window_width = 400
+    window_height = 150
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    position_top = int((screen_height - window_height) / 2)
+    position_left = int((screen_width - window_width) / 2)
+    root.geometry(f"{window_width}x{window_height}+{position_left}+{position_top}")
+    root.configure(bg="white")
+    root.title("RKOM Begehungs Excel Updater")
+    # Create a label widget
+    root.bind("<Configure>",on_resize)
+    file_label = tk.Label(root, text='Gewählte Datei:',bg="white",wraplength = window_width-50)
+    file_label.pack()
+    filename = tk.Label(root,text="Keine",bg="white",wraplength = window_width-50) #Keine
+    filename.pack()
+    
+    file_button = ttk.Button(root,text="Datei auswählen",command = lambda: select_file())
+    file_button.pack()
+    update_button = ttk.Button(root,text="Updaten",command = lambda: update_file(filename))
+    update_button.pack()
+    
+    output_label = tk.Label(root, text='',bg="white",wraplength = window_width-50)
+    output_label.pack()
+    # Start the GUI event loop
+    root.mainloop()
+
+main()
---
+++ planio/planio_queries.py
@@ -0,0 +1,50 @@
+import requests
+import pandas as pd
+
+URL_PLANIO_ISSUES = 'https://createcheng.planio.de/issues'
+
+def get_begehungsdaten(api_key: str, tracker_ids:str):
+    headers = {"X-Redmine-API-Key": api_key, "Content-Type": "application/json"}
+    begehungsdaten = []
+    offset = 0
+    i = 1
+    while True:
+        url = f'{URL_PLANIO_ISSUES}.json?tracker_id={tracker_ids}&limit=100&offset={offset}'
+        response = requests.get(url, headers=headers)
+        data: dict = response.json()
+        if not data["issues"]:
+            break
+        
+        for issue in data['issues']:
+            print(str(i)+"/"+str(data["total_count"]))
+            i = i+1
+            sachstand = ""
+            bemerkung = ""
+            #print(issue['id'],"\n","\n",issue["status"]['name'],"\n",issue["parent"]['id'],"\n",issue["subject"],"\n",issue["description"])
+            #print(issue['id'],"\n",issue["project"]['name'],"\n",issue["tracker"]['name'],"\n",issue["status"]['name'],"\n",issue["parent"]['id'],"\n",issue["subject"],"\n",issue["description"])
+            for custom_field in issue["custom_fields"]:
+                if custom_field["name"] == "Sachstand" and custom_field["value"] != "":
+                    sachstand = sachstand + (custom_field["value"]) + "\n"
+                if custom_field["name"] == "Ortstermin" and custom_field["value"] != "":
+                    bemerkung = custom_field["name"] + ": " + custom_field["value"] + "\n"
+                if custom_field["name"].endswith("Kontaktversuch") and custom_field["value"] != "":
+                    bemerkung = bemerkung + custom_field["name"] + ": " + custom_field["value"] + "\n"
+            issue_data = {
+                'issue_id': issue['id'],
+                'address': issue["subject"],
+                'status': issue["status"]['name'],
+                'sachstand': sachstand,
+                'bemerkung': bemerkung,
+                'description': issue["description"],
+            }
+            # for custom_field in issue.get('custom_fields'):
+            #     if custom_field['name'] == nachbesserungsgrund_fieldname:
+            #         issue_data.update({'nachbesserungsgrund': '; '.join(custom_field['value'])})
+            begehungsdaten.append(issue_data)
+
+        offset += 100
+        if offset > data["total_count"]:
+            break
+    df = pd.DataFrame(data=begehungsdaten)
+    #print(df.head(50))
+    return df
